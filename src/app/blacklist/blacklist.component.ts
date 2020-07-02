0c483c11d98af4a6a6eb119c3f58a5e26dc8000d
natchanon <natchanon@guymail.com>
2020-07-02 18:02:46 +0700

    guy
    02/07/2020 update 95%

--- src/app/blacklist/blacklist.component.ts
+++ src/app/blacklist/blacklist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import axios from "axios";
 import { environment } from './../../environments/environment';
 import Swal from 'sweetalert2'
+import { HttpClient, HttpRequest, HttpHeaders, HttpEvent } from '@angular/common/http';
 @Component({
   selector: 'app-blacklist',
   templateUrl: './blacklist.component.html',
@@ -10,20 +11,56 @@ import Swal from 'sweetalert2'
 export class BlacklistComponent implements OnInit {
   data: [];
   col: [];
-  datafromfile = []
+  value
+  today = new Date();
+  dataexecl: [];
+  colexecl: [];
+  selectedFiles: FileList;
+  currentFile: File;
   selectedCars3: [];
+  ////////msisdn table//////////////
   totalRecords = 10;
   first = 1
   last = 10
   totalpage = 5
-  constructor() { }
+  //////////msisdn table///////////
+
+  ////////execl table//////////////
+  totalRecordsexecl = 10;
+  firstexecl = 1
+  lastexecl = 10
+  totalpageexecl = 5
+  //////////execl table///////////
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-
+    axios.post(`${environment.URL_API}/Blacklistlist`)
+      .then(res => {
+        this.colexecl = res.data.column
+        this.dataexecl = res.data.data
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.response.data.error
+        })
+      })
+    let date: Date = new Date();
+    let dates = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    let dateTime = dates + ' ' + '23:59:59';
+    let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    this.value = dateTime
   }
+  selectCarWithButton(data) {
+    console.log(data)
+  }
+
   async add() {
     let param = {
-      "msisdn": (<HTMLInputElement>document.getElementById("MSISDN")).value
+      "msisdn": (<HTMLInputElement>document.getElementById("MSISDN")).value,
+      "user": sessionStorage.getItem('user')
     }
     await axios.post(`${environment.URL_API}/blacklistadd`, param)
       .then(res => {
@@ -93,20 +130,43 @@ export class BlacklistComponent implements OnInit {
 
 
 
-  uploadfile() {
-    console.log(JSON.stringify(this.datafromfile))
+  async uploadfile() {
+    let formData = new FormData();
+    this.currentFile = this.selectedFiles.item(0)
+    formData.append('file', this.currentFile);
+    formData.append('time', this.value);
+    formData.append('user', sessionStorage.getItem('user'));
+    await axios.post(`${environment.URL_API}/Blacklistfile`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then(res => {
+        Swal.fire({
+          icon: 'success',
+          text: "Success",
+        })
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message
+        })
+      })
+    // axios({
+    //   method: 'post',
+    //   url: `${environment.URL_API}/Blacklistfile`,
+    //   data: formData,
+    //   headers: { 'Content-Type': 'multipart/form-data' }
+    // })
+    //   .then(function (response) {
+    //     //handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (response) {
+    //     //handle error
+    //     console.log(response);
+    //   });
+
   }
-  changeListener(files: FileList) {
-    console.log(files);
-    if (files && files.length > 0) {
-      let file: File = files.item(0);
-      let reader: FileReader = new FileReader();
-      reader.readAsText(file);
-      this.datafromfile = []
-      reader.onload = (e) => {
-        let csv: string = reader.result as string;
-        this.datafromfile.push(csv)
-      }
-    }
+  changeListener(event) {
+    this.selectedFiles = event;
   }
 }
